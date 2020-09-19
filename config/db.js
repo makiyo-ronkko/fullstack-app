c83b96cc110a8920bd9189a68ae97f8ad29ea310
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-19 21:50:58 +0300

    Set up config

---
+++ config/db.js
@@ -0,0 +1,21 @@
+// mongoDB connection setup
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get(mongoURL);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.err(err.message);
+    // Exit process with failure
+    process.exist(1);
+  }
+};
+module.exports = connectDB;
