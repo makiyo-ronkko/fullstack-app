766f806280c17f60f734777a9951f66ebe6ad51e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-19 21:58:18 +0300

    Set up authentication for login

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+// authentication for login
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// middleware function
+module.exports = function (req, res, next) {
+  const token = req.header('auth-token');
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authentication dedined' });
+  }
+  try {
+    // Decode encrypted token
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    // Request object assign to user and assign the value to
+    // user payload (user id)
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is invalid.' });
+  }
+};
