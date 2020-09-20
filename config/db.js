34b6c02c98a16aef3f1be3cbcab9288634c1f9ed
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-20 20:58:15 +0300

    Fix mongoURL

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get(mongoURL);
+const db = config.get('mongoURL');
 
 const connectDB = async () => {
   try {
