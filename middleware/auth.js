0566cff9aff695938aa4decf199019ea5d213b5e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-23 15:54:58 +0300

    Fix req.header

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 // middleware function
 module.exports = function (req, res, next) {
-  const token = req.header('auth-token');
+  const token = req.header('x-auth-token');
   if (!token) {
     return res.status(401).json({ msg: 'No token, authentication dedined' });
   }
