5fe5e875ab3b1f84f0ef1669558bcc8e7b7454a3
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-24 22:52:23 +0300

    Style landing page

--- client/src/components/layout/Home/Home.js
+++ client/src/components/layout/Home/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
+    <div className='Home'>
+      <div className='light-overlay'>
+        <div className='Home-container'>
+          <h1>Daily Art Sharing</h1>
+          <p>
+            Share your arts and pictures daily and get ideas and inspiration
+            from peer artist.
+          </p>
+          <button>
+            <Link className='Home-button' to='/register'>
+              Register
+            </Link>
+          </button>
+          <button>
+            <Link className='Home-button' to='/login'>
+              Login
+            </Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
