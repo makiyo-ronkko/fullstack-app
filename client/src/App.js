43fb701aba6b8e12cd82ec2dac3ad4b1f08a73c1
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-24 16:05:23 +0300

    Import Provider and store

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,25 @@ import Home from './components/layout/Home/Home';
 import Navbar from './components/layout/Navbar/Navbar';
 import Signup from './components/admin/Signup/Signup';
 import Signin from './components/admin/Signin/Signin';
+
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 function App() {
   return (
-    <div className='App-container'>
-      <Router>
-        <Navbar />
-        <Route exact path='/' component={Home} />
-        <Switch>
-          <Route exact path='/register' component={Signup} />
-          <Route exact path='/login' component={Signin} />
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className='App-container'>
+        <Router>
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          <Switch>
+            <Route exact path='/register' component={Signup} />
+            <Route exact path='/login' component={Signin} />
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
