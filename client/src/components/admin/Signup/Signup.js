c08ffaff4edc75ea4ee3a55745c3c561a9d6070d
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-25 11:59:09 +0300

    Add input required alert logic and alert action

--- client/src/components/admin/Signup/Signup.js
+++ client/src/components/admin/Signup/Signup.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import './Signup.css';
 import { connect } from 'react-redux';
 import { register } from '../../../actions';
+import { alert } from '../../../actions/alert';
 import PropTypes from 'prop-types';
 
 const Signup = (props) => {
@@ -11,6 +12,7 @@ const Signup = (props) => {
     password: '',
     password2: '',
   });
+  const [submitted, setSubmitted] = useState(false);
 
   const input = (e) =>
     setData({
@@ -22,8 +24,10 @@ const Signup = (props) => {
     e.preventDefault();
     if (data.password !== data.password2) {
       console.log('Password do not match!');
+      props.alert('Password do not match');
     } else {
       props.register(data.name, data.email, data.password);
+      setSubmitted(true);
     }
   };
 
@@ -31,46 +35,61 @@ const Signup = (props) => {
     <Fragment>
       <div className='Signup-background'>
         <div className='container light-overlay'>
-          <form className='Form' onSubmit={(e) => submit(e)}>
-            <div className='Form-row'>
-              <h1>Registration</h1>
-            </div>
-            <div className='Form-row'>
-              <input
-                type='text'
-                placeholder='name'
-                name='name'
-                onChange={(e) => input(e)}
-              />
-            </div>
-            <div className='Form-row'>
-              <input
-                type='email'
-                placeholder='email'
-                name='email'
-                onChange={(e) => input(e)}
-              />
-            </div>
-            <div className='Form-row'>
-              <input
-                type='password'
-                placeholder='password'
-                name='password'
-                onChange={(e) => input(e)}
-              />
-            </div>
-            <div className='Form-row'>
-              <input
-                type='password'
-                placeholder='confirm password'
-                name='password2'
-                onChange={(e) => input(e)}
-              />
-            </div>
-            <div className='Form-row'>
-              <button type='submit'>Register</button>
-            </div>
-          </form>
+          <div className='container'>
+            <form className='Form' onSubmit={(e) => submit(e)}>
+              <div className='Form-row'>
+                <h1>Registration</h1>
+              </div>
+
+              <div className='Form-row'>
+                <input
+                  type='text'
+                  placeholder='Name'
+                  name='name'
+                  onChange={(e) => input(e)}
+                  value={data.name}
+                />
+              </div>
+              {submitted && !data.name && <p>Name is required</p>}
+
+              <div className='Form-row'>
+                <input
+                  type='email'
+                  placeholder='Email'
+                  name='email'
+                  onChange={(e) => input(e)}
+                  value={data.email}
+                />
+              </div>
+              {submitted && !data.email && <p>Email is required</p>}
+
+              <div className='Form-row'>
+                <input
+                  type='password'
+                  placeholder='Password'
+                  name='password'
+                  onChange={(e) => input(e)}
+                  value={data.password}
+                />
+              </div>
+              {submitted && !data.password && <p>Password is required</p>}
+
+              <div className='Form-row'>
+                <input
+                  type='password'
+                  placeholder='Confirm password'
+                  name='password2'
+                  onChange={(e) => input(e)}
+                  value={data.password2}
+                />
+              </div>
+              {submitted && !data.password2 && <p>Confirm your password</p>}
+
+              <div className='Form-row'>
+                <button type='submit'>Register</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </Fragment>
@@ -79,6 +98,7 @@ const Signup = (props) => {
 
 Signup.propTypes = {
   register: PropTypes.func.isRequired,
+  alert: PropTypes.func.isRequired,
 };
 
-export default connect(null, { register })(Signup);
+export default connect(null, { register, alert })(Signup);
