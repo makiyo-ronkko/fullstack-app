6680827e84acdeff17975556942804dea8f807bd
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-25 13:47:30 +0300

    Create types: SIGNIN_SUCCESS and SIGNIN_FAIL

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,8 @@
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_FAIL = 'SIGNUP_FAIL';
 
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
+export const SIGNIN_FAIL = 'SIGNIN_FAIL';
+
 export const ALERT_FAIL = 'ALERT_FAIL';
 export const ALERT_CLEAR = 'ALERT_CLEAR';
