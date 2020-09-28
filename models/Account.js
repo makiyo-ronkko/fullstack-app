afefe871e4af68c5507fdd87be3521dfc0b057a7
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-28 17:13:11 +0300

    Create Account model

---
+++ models/Account.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const AccountSchema = new mongoose.Schema({
+  appuser: {
+    type: mongoose.Schema.Types.ObjectId, // to connect to registered Appuser
+    ref: 'appuser', // reference to appuser model
+  },
+  intro: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  loaction: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('account', AccountSchema);
