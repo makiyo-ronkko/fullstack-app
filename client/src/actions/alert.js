e4afe6b215c59ba8791720326e36f98d48c80df0
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-28 11:53:25 +0300

    ALERT_CELAR with SetTimeout

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,11 +2,15 @@ import { ALERT_FAIL, ALERT_CLEAR } from './types';
 import { v4 as uuidv4 } from 'uuid';
 
 // Dispatch alert message and type
-export const alert = (message) => (dispatch) => {
+export const alert = (message, timeout = 3000) => (dispatch) => {
   const id = uuidv4();
   // call ALERT_FAIL reducer
   dispatch({
     type: ALERT_FAIL,
     payload: { message, id },
   });
+
+  // to avoid multiple alert messages on click
+  // to remove alert, payload is only id
+  setTimeout(() => dispatch({ type: ALERT_CLEAR, payload: id }), timeout);
 };
