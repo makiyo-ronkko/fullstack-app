6840dd999dd910ec1b81f9991c895950889f8fc2
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-01 16:07:45 +0300

    Update styling

--- client/src/components/accountForm/AccountForm.js
+++ client/src/components/accountForm/AccountForm.js
@@ -15,44 +15,43 @@ const AccountForm = () => {
   return (
     <div>
       <div className='container'>
-        <form className='Form'>
+        <div className='AccountForm-title'>
+          <h1>Welcome "user" and "username"!</h1>
+        </div>
+        <form className='AccountForm'>
           <div className='Form-row'>
-            <h1>Your Profile Information</h1>
+            <h1>Create Profile</h1>
           </div>
 
           <label for='intro'>Short introduction about yourself</label>
-          <div className='Form-row'>
-            <input
-              type='text'
-              placeholder='About yourself'
-              name='intro'
-              onChange={input}
-              value={data.intro}
-            />
-          </div>
+          <input
+            type='textarea'
+            placeholder='About yourself'
+            name='intro'
+            onChange={input}
+            value={data.intro}
+          />
+          <hr />
 
           <label for='website'>Your website:</label>
-          <div className='Form-row'>
-            <input
-              type='url'
-              placeholder='https://example.com'
-              pattern='https//.*'
-              name='website'
-              onChange={input}
-              value={data.website}
-            />
-          </div>
+          <input
+            type='url'
+            placeholder='https://example.com'
+            pattern='https//.*'
+            name='website'
+            onChange={input}
+            value={data.website}
+          />
+          <hr />
 
           <label for='location'>Where do you live?</label>
-          <div className='Form-row'>
-            <input
-              type='text'
-              placeholder='location'
-              name='location'
-              onChange={input}
-              value={data.location}
-            />
-          </div>
+          <input
+            type='text'
+            placeholder='location'
+            name='location'
+            onChange={input}
+            value={data.location}
+          />
 
           <div className='Form-row'>
             <button type='submit'>Create</button>
