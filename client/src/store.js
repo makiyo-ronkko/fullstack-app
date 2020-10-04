592b3a68dba1fe2cfeaad50efda42464c9602eb3
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-04 20:13:58 +0300

    Remove localStorage management from redux and add store.subscribe

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import setAuthToken from './utils/setAuthToken';
 
 const initialState = {};
 
@@ -12,5 +13,19 @@ const store = createStore(
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
+
+// store subscribe(listener)
+// user token in localStorage
+// initialize current state
+let currState = store.getState();
+
+store.subscribe(() => {
+  let preState = currState;
+  currState = store.getState();
+  if (preState.auth.token !== currState.auth.token) {
+    const token = currState.auth.token;
+    setAuthToken(token);
+  }
+});
 
 export default store;
