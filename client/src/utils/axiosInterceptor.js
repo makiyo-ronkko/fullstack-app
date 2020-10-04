b23bb20c744914095a74664c84ac5d366fda25e6
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-04 20:16:20 +0300

    Create axios instance and add axios interceptors before making request

---
+++ client/src/utils/axiosInterceptor.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import store from '../store';
+import { LOGOUT } from '../actions/types';
+
+const instance = axios.create({
+  baseURL: '/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptors are methods which are triggered before the main method.
+// response interceptor: this is called before the promise is completed and the data is received
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.data.message === 'Token is invalid.') {
+      store.dispatch({ type: LOGOUT });
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,17 @@
+// Helper function
+// If there is a token, add to headers
+// If not, delete from headers
+
+import axiosInterceptor from './axiosInterceptor';
+
+const setAuthToken = (token) => {
+  if (token) {
+    axiosInterceptor.defaults.headers.common['x-auth-token'] = token; // add token to headers
+    localStorage.setItem('token', token); // save to localStorage
+  } else {
+    delete axiosInterceptor.defaults.headers.common['x-auth-token'];
+    localStorage.removeItem('token'); // remove token from localStorage
+  }
+};
+
+export default setAuthToken;
