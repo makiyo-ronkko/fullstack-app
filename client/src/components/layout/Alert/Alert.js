40d18920a25b3d03fdd8dcfe4ee9c22751c5f1bb
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-05 17:16:44 +0300

    Update styling

--- client/src/components/layout/Alert/Alert.js
+++ client/src/components/layout/Alert/Alert.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './Alert.css';
 
 const Alert = (props) =>
   props.alerts !== null &&
   props.alerts.length > 0 &&
   props.alerts.map((alert) => (
     <div key={alert.id} className='Alert'>
-      <p>{alert.message}</p>
+      <div className={`Alert-content Alert-${alert.type}`}>{alert.message}</div>
     </div>
   ));
 Alert.propTypes = {
