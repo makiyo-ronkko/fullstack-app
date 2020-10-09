04a45de51b2bf3006457676bcf6907ec781ac906
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-09 17:17:35 +0300

    Remove Fragment

--- client/src/components/posts/PostList.js
+++ client/src/components/posts/PostList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PostItem from './PostItem';
 import { connect } from 'react-redux';
