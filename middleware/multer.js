a68bd4b193163c5ebb8b80617aea3f826c0c48a3
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-14 15:29:08 +0300

    Create multer and cloudinary middleware

---
+++ middleware/multer.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+module.exports = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    },
+  }),
+  //   storage: multer.memoryStorage(),
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.match(/jpe|jpeg|png|gif$i/)) {
+      cb(new Error('File is not supported'), false);
+      return;
+    }
+
+    cb(null, true);
+  },
+});
