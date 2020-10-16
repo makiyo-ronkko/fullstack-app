cb41e5ff7719259ee4c3d4cdd43318180247456e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-16 23:54:03 +0300

    Create Footer component

---
+++ client/src/components/layout/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <div className='copyright'>
+        <p>Copyright &copy; 2020 Makiyo Rönkkö</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
