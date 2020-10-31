b3f904c9dfd416e04cd1492b4717cbaba0b0b355
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-31 09:29:17 +0200

    Setup heroku app deployment

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ app.use('/auth', require('./routes/api/auth'));
 app.use('/profile', require('./routes/api/profile'));
 app.use('/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// sEt static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
